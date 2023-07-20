94c9fdc29274f4874252749918464a228a301323
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-07-21 08:49:54 +0900

    rails g migrationでauthorのカラム追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,8 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
+      p "**********"
+      p params
       params.require(:book).permit(:title, :memo)
     end
 end
